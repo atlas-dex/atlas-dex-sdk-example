c282ff9ad980103c1d40c48caaf8001a47c09ac7
ideo-faisal-ali <108666159+ideo-faisal-ali@users.noreply.github.com>
2022-09-28 16:29:13 +0500

    fixes
    udapted sol to sol and readme and .env example

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # Atlas Dex Sdk Examples
+
+The demo of our sdk executes following use cases
+1. SOL to SOL 
+2. EVM to EVM same Chain
+3. EVM to EVM cross chain
+
+# Example Setup
+
+Clone the repository https://github.com/atlas-dex/atlas-dex-sdk-example.git
+Rename .evn-example file to .env  and update the required keys
+
+Use follosing value for base url.
+```bash
+BASE_URL =
+```
+
+# 1. SOLANA to SOLANA
+
+## Get Quote /routes 
+
+Sample output
+```bash
+{
+    "routes": [
+        {
+            "route_id": "92519a24-897d-4ef7-a644-485bcd3dee54",
+            "inAmount": 10000000,
+            "outAmount": 2162734017,
+            "priceImpact": -0.02083,
+            "steps": [
+                "SWAP_SOLANA"
+            ]
+        },
+        {
+            "route_id": "38f9d67f-655f-40d7-81df-a8dcf03954fd",
+            "inAmount": 10000000,
+            "outAmount": 2149140000,
+            "priceImpact": -0.021,
+            "steps": [
+                "SWAP_SOLANA"
+            ]
+        }
+    ],
+    "errors": []
+}
+```
+
+## Get Swap Transaction /swap
+
+Sample Output
+```bash
+{
+    "message": "Sign Transaction and Send Hash",
+    "data": {
+        "chainId": 101,
+        "txs": [
+            "AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAkcWWHrY2Y38rvdTNsEUXVekQfjMYXLnBeF8e4NhUhnkUQQRZUeQo8PyLxkYY35mL/ZO30pPSgcWMDpUuJ3auZJeCZl97b2E1RAaX2mnY1JBa9hP8tDBemJ1Evd0lG0Z6DjIgMC9HSScDlyCgvFR24IkeMXCbrbmxpqme7K4Mw3Rz83tnZZBaMu5w0BJfnoiJn9S8dN64WSFd20MpgcZB9Uezs8T6yweJbco6S/Fvq2FFGFCTWEM0i0IAsCdSSMEwNOPGnLo1P0ScfipO1Y73yILBN4+2TLqUrvHsYqofZz7dtEovP2eEMKGjmNw1Pl1qgHkDWp5qqPkTB0HT7wqD53JE/n1DDUmigeGV0JMbPYFE30QbQA2iOiRfJti1wKWGExgPvMeuEPVakRIW6i24aJ/aKXTMh0in03COtcvSTbsjGMRVISFahxhuGAwR/o78Ywit0jRqe53fp8vsOkAeGEQ5bpryrsiIqMxNfcdyEOT5z/RL/qVKQWxmjLtvpWB589qQr+ZPqR4vr0S15SwJpk00Mh7+0RROcaLb7JiR4nbhe0X1SPJO1bPYAZWLDoXLtvuSVL/rHU4KO9hdcsKWdM4br59qSe4EI0mmAyq8r2INUC+8eJd0ipfMSCDr7XA+SJtwhAH1WNoes8VRx5oIc93ubdcemmbTJiKeduRvioE8PltIcV08mqYFjPBIuzqoyjOaBdhADcfhxB+6xDEdFvFvAyXgwHePZZ0pHB7oVJlsQeP6FElkKKh7BXI/aG9SFf8sJtSKAO2mohApv3PxTyLHEgFdrP8M9uD69577Zrulgo/QYQdjEBkKvEPyKzUVmw7CxmxAcftA8ROAKLalNn1kFXsFgPMcX85EpiWC28+deO51lDoISjk7NQNo0iiZMIS9lJxDYCwz8gd5DtFqNSTKG5l1zxIaKpDP/sffi2is2FDy1uAqR6+CTQmradxC1wyyjL+iSft+5XudJWwSdi7/w8v0x/jjkc1tySR7wSP2lhs0TbmojPCJbjwin0QIy0BHnVH6nNSvb3qwqwbkgtxkwyjapZJyq+L06YgldeDngFLso2h2OllV9ic05A53KXg4jsK9j1DGvyI/ItJHtCrgbd9uHXZaGT2cvhRs7reawctIXtX1s3kTqM9YV+/wCpBwcvNCrmMP6T7EmFZ5Mxx1iQuIm2pm0eCo0luEtoYIMPjZeChayRoaiSZ051Ys2JFnUCFbfSe/rE8AK+UkdNVgMYAgMPCORVuXBOT00CGAsZGxoTABEPBgcJEhof+Dzi16g3xwGAlpgAAAAAAAAAAAAAAAAAABgTFRoQFAILBBYIAQ4NBQoXDwwAAxJF42Jd7crfjADbskOAAAAAAAA="
+        ]
+    },
+    "action": "SWAP_SOLANA",
+    "error": [
+        null
+    ]
+}
+```
--- sol_sol.js
+++ sol_sol.js
@@ -1,119 +1,75 @@
-const axios = require('axios');
-const  {
-    Connection,Keypair,Transaction
-  } = require('@solana/web3.js');
+const axios = require("axios");
+const { Connection, Keypair, Transaction, sendAndConfirmTransaction } = require("@solana/web3.js");
 const { Wallet } = require("@project-serum/anchor");
-const bs58 = require ('bs58');
-const SOLANA_WALLET_PUBLIC_ADDRESS = process.env.SOLANA_WALLET_PUBLIC_ADDRESS;
+const bs58 = require("bs58");
 const SOLANA_WALLET_PRIVATE_ADDRESS = process.env.SOLANA_WALLET_PRIVATE_ADDRESS;
-const SOLANA_RPC = 'https://autumn-palpable-hexagon.solana-mainnet.discover.quiknode.pro/';
+const SOLANA_RPC = "https://api.mainnet-beta.solana.com";
+//Declare this key in the .env and check readme for the url
+const BASE_URL = process.env.BASE_URL;
+console.log(BASE_URL);
+const performSolToSolSWAP = async function performSolToSolSWAP() {
+  try {
+    //Setup a Solana Wallet
+    //Create a .env file and use the key name SOLANA_WALLET_PRIVATE_ADDRESS=YOUR_SOLANA_ACCOUNT_PRIVATE_KEY
+    const wallet = await new Wallet(
+      Keypair.fromSecretKey(bs58.decode(SOLANA_WALLET_PRIVATE_ADDRESS || ""))
+    );
 
-const performSolToSolSWAP =  async function performSolToSolSWAP () {
-    try{
-        let routeData
-        try {
-            const options = {
-                method: 'POST',
-                url:   'http://35.179.82.212:4000/api/routes',
-                data: {
-                    fromAmount: '0.01',
-                    fromChainId: 101,
-                    fromTokenAddress: 'HJbNXx2YMRxgfUJ6K4qeWtjatMK5KYQT1QnsCdDWywNv',
-                    toChainId: 101,
-                    toTokenAddress: 'EjmyN6qEC1Tf1JxiG1ae7UTJhUxSwk1TCWNWqxWV4J6o',
-                    slippage: 0.5,
-                    fromAccountAddress: process.env.SOLANA_WALLET_PUBLIC_ADDRESS, //SOLANA
-                    toAccountAddress: process.env.SOLANA_WALLET_PUBLIC_ADDRESS, //SOLANA
-                  }
-              };
-             const data = await axios(options);
-             routeData = data.data;
-        }
-        catch (e) {
-            console.log('error at http call', e);
-        }
+    // Get Quote Data from /routes endpoint
+    const { data: quoteData } = await axios({
+        method: "POST",
+        url: `${BASE_URL}/routes`,
+        data: {
+          fromAmount: "0.01",
+          fromChainId: 101,
+          fromTokenAddress: "HJbNXx2YMRxgfUJ6K4qeWtjatMK5KYQT1QnsCdDWywNv",
+          toChainId: 101,
+          toTokenAddress: "EjmyN6qEC1Tf1JxiG1ae7UTJhUxSwk1TCWNWqxWV4J6o",
+          slippage: 10,
+          fromAccountAddress: wallet.publicKey.toBase58(), //SOLANA
+          toAccountAddress: wallet.publicKey.toBase58(), //SOLANA
+        },
+      });
 
-        
-        //Data is an array and jupiter return 3 routes we are using the first route
-        const quote = routeData.routes[0];
-        const routeId = quote.route_id;
-        //This call will perform the first step of a route and return the transaction
-        let firstStepTransaction ;
-        try {
-            const options = {
-                method: 'POST',
-                url:   'http://35.179.82.212:4000/api/swap',
-                data: { route_id: routeId, walletkey: SOLANA_WALLET_PUBLIC_ADDRESS }
-              };
-             const data = await axios(options);
-             firstStepTransaction = data.data;
-        }
-        catch (e) {
-            console.log('error at http call', e);
-        }
-
-        //Get solana RPC connection
-        const connection = new Connection(SOLANA_RPC);
-        //Setup Solana Wallet
-        console.log('seckey', SOLANA_WALLET_PRIVATE_ADDRESS);
-        const wallet = await new Wallet(
-            Keypair.fromSecretKey(
-                bs58.decode((SOLANA_WALLET_PRIVATE_ADDRESS) || ''),
-            ),
-        );
-        console.log(firstStepTransaction);
-        let transaction;
-        try {
-            const { setupTransaction, swapTransaction, cleanupTransaction } = firstStepTransaction.data[0];
-            transaction = new Transaction();
+    //Data is an array and jupiter return 3 routes we are using the first route
+    const quote = quoteData.routes[0];
     
-            for (let serializedTransaction of [
-                setupTransaction,
-                swapTransaction,
-                cleanupTransaction,
-            ].filter(Boolean)) {
-                // get transaction object from serialized transaction
-                transaction.add(
-                    Transaction.from(Buffer.from(serializedTransaction, 'base64')),
-                );
-            }
+    // You have a quote now pass the below data to /swap endpoint to get transactions data for Swap Solana
+    const { data: swapData } = await axios({
+      method: "POST",
+      url: `${BASE_URL}/swap`,
+      data: {
+        route_id: quote.route_id,
+        walletkey: wallet.publicKey.toBase58(),
+        action: "SWAP_SOLANA",
+      },
+    });
+
+    //Get solana RPC connection
+    const connection = new Connection(SOLANA_RPC);
+
+    //Get the transactions for swap solana
+    const { txs } = swapData.data;
+
+    //loop each transaction and broadcast
+    for (let serializedTransaction of txs) {
+      
+      const transaction = Transaction.from(
+        Buffer.from(serializedTransaction, "base64")
+      );
+      const hash = await sendAndConfirmTransaction(connection,
+        transaction,
+        [wallet.payer],
+        {
+          skipPreflight: false,
         }
-        catch (e) {
-            console.log('error at swap sol transaction: ', e);
-        }
-        const txid = await connection.sendTransaction(
-            transaction,
-            [wallet.payer],
-            {
-              skipPreflight: true,
-            },
-          );
-          const hash = txid
-          await connection.confirmTransaction(txid);
-          let result
-          try {
-            const options = {
-                method: 'POST',
-                url:   'http://35.179.82.212:4000/api/swap',
-                data: {
-                    route_id: routeId,
-                    walletkey: SOLANA_WALLET_PUBLIC_ADDRESS,
-                    hash: hash,
-                  }
-              };
-             const data = await axios(options);
-             result = data.data;
-        }
-        catch (e) {
-            console.log('error at http call', e);
-        }
-          console.log('Swap Performed successfully', result);
+      );
+      console.log(`https://solscan.io/tx/${hash}`);
     }
-    catch(e) {
-        console.log(e);
-    }
-    
-}
+  } catch (e) {
+    console.log('Error: ',e);
+  }
+};
 module.exports = {
-    performSolToSolSWAP
-}
+  performSolToSolSWAP,
+};
